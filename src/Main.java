972d90096c86cec6905821f8397fb354edec5066
Zara8170 <wlsgusan123@gmail.com>
2025-07-27 20:09:18 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.*;
 
-        
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(fibonacci(n));
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int a = 0;
+        int b = 1;
+        int result = 0;
+
+        for (int i = 2; i <= n; i++) {
+            result = a + b;
+            a = b;
+            b = result;
+        }
+
+        return result;
     }
 }
