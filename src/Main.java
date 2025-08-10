dc8a712229806146cad2b2a162db1f8469f498ac
Zara8170 <wlsgusan123@gmail.com>
2025-08-10 12:33:42 +0900

    백준 11729번 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,33 @@
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+ 
+	public static StringBuilder sb = new StringBuilder();
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+ 
+		sb.append((int) (Math.pow(2, N) - 1)).append('\n');
+ 
+		Hanoi(N, 1, 2, 3);
+		System.out.println(sb);
+ 
+	}
+ 
+	public static void Hanoi(int N, int start, int mid, int to) {
+		if (N == 1) {
+			sb.append(start + " " + to + "\n");
+			return;
+		}
 
-        System.out.println(fibonacci(n));
-    }
-
-    public static int fibonacci(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-
-        int a = 0;
-        int b = 1;
-        int result = 0;
-
-        for (int i = 2; i <= n; i++) {
-            result = a + b;
-            a = b;
-            b = result;
-        }
-
-        return result;
-    }
+		Hanoi(N - 1, start, to, mid);
+    
+		sb.append(start + " " + to + "\n");
+    
+		Hanoi(N - 1, mid, start, to);
+	}
 }
