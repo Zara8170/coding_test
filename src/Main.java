226d960dc570748e46d2b6e695759b48e24e37db
Zara8170 <wlsgusan123@gmail.com>
2025-10-02 14:14:25 +0900

    프로그래머스 문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+class Solution {
+    public int solution(int price) {
+        final int SIBMAN = 100000;
+
+        if (price >= 5 * SIBMAN) {          // 20%
+            return price * 80 / 100;        // price * (100 - 20) / 100
+        } else if (price >= 3 * SIBMAN) {   // 10%
+            return price * 90 / 100;
+        } else if (price >= SIBMAN) {       // 5%
+            return price * 95 / 100;
+        } else {
+            return price;
+        }
     }
 }
