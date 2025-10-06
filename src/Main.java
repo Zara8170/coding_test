200b786b3fe50dbec6fc4d702f66d7a380594b4c
Zara8170 <wlsgusan123@gmail.com>
2025-10-06 16:21:42 +0900

    코테 스터디

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,5 @@
 class Solution {
     public int solution(int price) {
         final int SIBMAN = 100000;
-
-        if (price >= 5 * SIBMAN) {          // 20%
-            return price * 80 / 100;        // price * (100 - 20) / 100
-        } else if (price >= 3 * SIBMAN) {   // 10%
-            return price * 90 / 100;
-        } else if (price >= SIBMAN) {       // 5%
-            return price * 95 / 100;
-        } else {
-            return price;
-        }
     }
 }
