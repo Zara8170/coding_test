e5b029d7b01ce0a94992884bbc6be0983a663a19
Zara8170 <wlsgusan123@gmail.com>
2025-10-08 23:26:00 +0900

    format 테스트

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,30 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        int[] arr = new int[N];
-        for(int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+        if((a < 1 || a > 100) || (b < 1 || b > 100)) {
+            System.out.println("Invalid input");
+            return;
         }
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
         
+        System.out.printf("%d + %d = %d\n", a, b, a + b);
+        System.out.printf("%d - %d = %d\n", a, b, a - b);
+        System.out.printf("%d * %d = %d\n", a, b, a * b);
+        System.out.printf("%d / %d = %d\n", a, b, a / b);
+        System.out.printf("%d %% %d = %d\n", a, b, a % b);
+        
+        System.out.printf("%d + %d = %d\n", a, b, a + b);
+        System.out.printf("%d - %d = %d\n", a, b, a - b);
+        System.out.printf("%d * %d = %d\n", a, b, a * b);
+        System.out.printf("%d / %d = %d\n", a, b, a / b);
+        System.out.printf("%d %% %d = %d\n", a, b, a % b);
     }
 }
