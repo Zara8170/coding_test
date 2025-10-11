83e5c4d5d2ac88c8051b68ab4d8e3ebd63beb0bc
Zara8170 <wlsgusan123@gmail.com>
2025-10-11 15:26:45 +0900

    코테 문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
-import java.util.Scanner;
+class Solution {
+    public int solution(int[][] signals) {
+        int n = signals.length;
+        int limit = 100000;
 
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String answer = sc.nextLine();
-        int n = sc.nextInt();
-        
-        String result = answer.substring(answer.length() - n);
-        System.out.println(result);
+        int[] cycle = new int[n];
+        for (int i = 0; i < n; i++) {
+            cycle[i] = signals[i][0] + signals[i][1] + signals[i][2];
+        }
+
+        for (int t = 1; t <= limit; t++) {
+            boolean allYellow = true;
+            for (int i = 0; i < n; i++) {
+                int pos = (t - 1) % cycle[i] + 1;
+                int g = signals[i][0];
+                int y = signals[i][1];
+                if (pos < g + 1 || pos > g + y) {
+                    allYellow = false;
+                    break;
+                }
+            }
+            if (allYellow) return t;
+        }
+
+        return -1;
     }
 }
