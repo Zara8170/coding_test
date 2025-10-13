9de971e440db0a26204af3d5ad88fb68faae4af3
Zara8170 <wlsgusan123@gmail.com>
2025-10-13 14:20:28 +0900

    PCCE 기출문제 공원 문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,57 @@
-class Solution {
-    public int solution(int[][] signals) {
-        int n = signals.length;
-        int limit = 100000;
+import java.util.Scanner;
+import java.util.Arrays;
 
-        int[] cycle = new int[n];
-        for (int i = 0; i < n; i++) {
-            cycle[i] = signals[i][0] + signals[i][1] + signals[i][2];
+public class Main {
+    static int n = 0;
+    static int m = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] mats = new int[n];
+        for(int i = 0; i < n; i++) {
+            mats[i] = sc.nextInt();
         }
+        
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        String[][] park = new String[rows][cols];
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                park[i][j] = sc.next();
+            }
+        }
+        
+        Arrays.sort(mats);
 
-        for (int t = 1; t <= limit; t++) {
-            boolean allYellow = true;
-            for (int i = 0; i < n; i++) {
-                int pos = (t - 1) % cycle[i] + 1;
-                int g = signals[i][0];
-                int y = signals[i][1];
-                if (pos < g + 1 || pos > g + y) {
-                    allYellow = false;
-                    break;
+        n = park.length;
+        m = park[0].length;
+
+        for(int i = mats.length - 1; i >= 0; i--) {
+            for(int x = 0; x < n; x++) {
+                for(int y = 0; y < m; y++) {
+                    if(checkSquare(park, mats[i], x, y)) {
+                        System.out.println(mats[i]);
+                    }
                 }
             }
-            if (allYellow) return t;
         }
 
-        return -1;
+        System.out.println(-1);
+    }
+
+    public static boolean checkSquare(String[][] park, int size, int x, int y) {
+        if(x + size > n || y + size > m) {
+            return false;
+        }
+
+        for(int i = x; i < x + size; i++) {
+            for(int j = y; j < y + size; j++) {
+                if(!park[i][j].equals("-1")) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
